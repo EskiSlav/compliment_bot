59f6d47018ec19f146992c82d2e3bcd7c59a6b60
Viacheslav Kozachok <viak@opinov8.com>
2023-02-26 15:43:35 +0200

    fixes

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import boto3
 
 
@@ -5,14 +7,22 @@ def get_ssm_parameter_value(parameter_string, ssm_client=None):
     if ssm_client is None:
         ssm_client = boto3.client('ssm')
 
-    parameter = ssm_client.get_parameter(Name=parameter_string, WithDecryption=True)
+    parameter = ssm_client.get_parameter(
+        Name=parameter_string, WithDecryption=True,
+    )
 
     return parameter['Parameter']['Value']
 
 
-def lambda_handler(event, context):
-    get_ssm_parameter_value("/config/bot/api_token")
-    
-    return { 
-        'status': 200
+def main():
+    api_token = get_ssm_parameter_value('/config/bot/api_token')
+
+    return {
+        'status': 200,
+        'value': api_token[:5] + '*' * len(api_token[5:]),
     }
+
+
+def lambda_handler(event, context):
+
+    return main()
