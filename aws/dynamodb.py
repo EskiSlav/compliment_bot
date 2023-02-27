9895670c2e627e8f0172f90da8abec00987d2d8f
Viacheslav Kozachok <viak@opinov8.com>
2023-02-27 17:50:51 +0200

    User is added now to the Users DynamoDB

--- aws/dynamodb.py
+++ aws/dynamodb.py
@@ -4,19 +4,40 @@ from hashlib import sha1
 
 import boto3
 
+from .models import Compliment
+from tg.models import User
+
 
 class DynamoDB:
+    USERS_TABLE = 'users'
+    COMPLIMENTS_TABLE = 'compliments'
+
     def __init__(self) -> None:
         self.dynamodb = boto3.resource('dynamodb', region_name='eu-west-2')
 
-    def put_compliment(self, compliment, translated_compliment) -> None:
-        table = self.dynamodb.Table('compliments')
+    def put_compliment(self, compliment: Compliment) -> bool:
+        table = self.dynamodb.Table(DynamoDB.COMPLIMENTS_TABLE)
         response = table.put_item(
             Item={
-                'id': sha1(compliment).hexdigest(),
-                'compliment': compliment,
-                'translated_compliment': translated_compliment,
+                'id': sha1(compliment.compliment).hexdigest(),
+                'compliment': compliment.compliment,
+                'translated_compliment': compliment.compliment_ua,
             },
         )
         status_code = response['ResponseMetadata']['HTTPStatusCode']
-        return status_code
+        return bool(status_code)
+
+    def put_user(self, user: User) -> bool:
+        table = self.dynamodb.Table(DynamoDB.USERS_TABLE)
+        response = table.put_item(
+            Item={
+                'id': user.id,
+                'is_bot': user.is_bot,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'username': user.username,
+                'language_code': user.language_code,
+            },
+        )
+        status_code = response['ResponseMetadata']['HTTPStatusCode']
+        return bool(status_code)
--- lambda_handler.py
+++ lambda_handler.py
@@ -2,21 +2,24 @@ from __future__ import annotations
 
 import json
 
-import aws
+from aws.ssm import get_ssm_parameter_value
+from func.func import put_user_to_the_table
 from tg.bot import Bot
 from tg.models import Update
-# from aws.ssm import get_ssm_parameter_value
-# from aws.translate import Translator
+
+default_message = 'Your message is accepted! Wait 9:00 Kyiv '
+default_message += 'time for your compliment! Have a great day'
 
 
 def main(event, context):
-    api_token = aws.get_ssm_parameter_value('/config/bot/api_token')
+    api_token = get_ssm_parameter_value('/config/bot/api_token')
     print(event)
 
     update = Update(json.loads(event['body']))
+    put_user_to_the_table(update.message.from_user)
 
     bot = Bot(api_token)
-    bot.send_message(update.message.chat.id, 'Wooooow!!!')
+    bot.send_message(update.message.chat.id, default_message)
 
     body = json.dumps({
         'status': 200,
@@ -31,5 +34,4 @@ def main(event, context):
 
 
 def lambda_handler(event, context):
-
     return main(event, context)
