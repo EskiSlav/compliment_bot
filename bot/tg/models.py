10b68550b1fa6d9fc2b5e1bd046ea3fdb4b5bd40
Viacheslav Kozachok <viak@opinov8.com>
2023-03-10 03:17:28 +0200

    Fixed inserting user without lastname

--- bot/tg/models.py
+++ bot/tg/models.py
@@ -4,11 +4,11 @@ from __future__ import annotations
 class User:
     def __init__(self, user: dict) -> None:
         self.id: int = user['id']
-        self.is_bot: bool = user['is_bot']
-        self.first_name: str = user['first_name']
-        self.last_name: str = user['last_name']
-        self.username: str = user['username']
-        self.language_code: str = user['language_code']
+        self.is_bot: bool = user.get('is_bot')
+        self.first_name: str = user.get('first_name')
+        self.last_name: str = user.get('last_name')
+        self.username: str = user.get('username')
+        self.language_code: str = user.get('language_code')
 
     def __repr__(self) -> str:
         return str(self.__dict__)
@@ -16,11 +16,11 @@ class User:
 
 class Chat:
     def __init__(self, chat: dict) -> None:
-        self.id: int = chat['id']
-        self.first_name: str = chat['first_name']
-        self.last_name: str = chat['last_name']
-        self.username: str = chat['username']
-        self.type: str = chat['type']
+        self.id: int = chat.get('id')
+        self.first_name: str = chat.get('first_name')
+        self.last_name: str = chat.get('last_name')
+        self.username: str = chat.get('username')
+        self.type: str = chat.get('type')
 
     def __repr__(self) -> str:
         return str(self.__dict__)
@@ -28,10 +28,10 @@ class Chat:
 
 class Message:
     def __init__(self, message: dict) -> None:
-        self.from_user = User(message['from'])
-        self.chat = Chat(message['chat'])
-        self.date: int = message['date']
-        self.text: int = message['text']
+        self.from_user = User(message.get('from'))
+        self.chat = Chat(message.get('chat'))
+        self.date: int = message.get('date')
+        self.text: int = message.get('text')
 
     def __repr__(self) -> str:
         return str(self.__dict__)
@@ -40,7 +40,7 @@ class Message:
 class Update:
     def __init__(self, update: dict) -> None:
         self.update_id = update['update_id']
-        self.message = Message(update['message'])
+        self.message = Message(update.get('message'))
 
     def __repr__(self) -> str:
         return str(self.__dict__)
